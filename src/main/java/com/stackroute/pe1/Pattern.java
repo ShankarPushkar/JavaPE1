d0ab9b01b63889ccb7e6b357f5c344281a6be8ca
Pushkar.Shankar <pushkar.shankar7@gmail.com>
2019-07-12 12:42:17 +0530

    Modified pattern java

--- src/main/java/com/stackroute/pe1/Pattern.java
+++ src/main/java/com/stackroute/pe1/Pattern.java
@@ -3,16 +3,20 @@ package com.stackroute.pe1;
 import java.util.Scanner;
 
 public class Pattern {
-    public int patternBuider(Integer number) {
+    public int[] patternBuider(int number) {
         int temp=0;
+        int counter=0;
+        int[] sendback=new int[10];
         if (number == 0) {
-            return 0;
+            return sendback;
         }
         for (int i = 1; i <= number; i++) {
             for (int j = 1; j <= i; j++) {
+                sendback[counter]=j;
+                counter++;
 
             }
         }
-        return temp;
+        return sendback;
     }
 }
