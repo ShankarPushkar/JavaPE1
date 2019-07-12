e0b0894722fd9494d323780c1d51a0efcfa0766d
Pushkar.Shankar <pushkar.shankar7@gmail.com>
2019-07-12 14:57:00 +0530

    Added StringReverse Test file

--- src/test/java/com/stackroute/pe1/StringReverseTest.java
+++ src/test/java/com/stackroute/pe1/StringReverseTest.java
@@ -7,16 +7,22 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StringReverseTest {
-
+    StringReverse stringReverse;
     @Before
     public void setUp()  {
+        stringReverse=new StringReverse();
     }
 
     @After
     public void tearDown() {
+        stringReverse=null;
     }
 
     @Test
-    public void stringReverser() {
+    public void givenStringShouldReturnReverse() {
+        //act
+        String actualResult=stringReverse.stringReverser("pushkar");
+        //assert
+        assertEquals("rakhsup",actualResult);
     }
 }
