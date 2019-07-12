e7c075cbd1a7cc28427a5362a124d51bb239d780
Pushkar.Shankar <pushkar.shankar7@gmail.com>
2019-07-12 12:15:38 +0530

    Added GuessNumber Test and made changes to GuessNumber Test Java files

--- src/main/java/com/stackroute/pe1/GuessNumber.java
+++ src/main/java/com/stackroute/pe1/GuessNumber.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 import java.util.Random;
 public class GuessNumber {
 
-    public String numberGuesser(int num, int user) {
+    public String numberGuesser(int number, int userinput) {
 
-            if (user<num) return ("Number guessed is less than original number");
-            else if (user>num) return ("Number guessed is more than original number");
+            if (userinput<number) return ("Number guessed is less than original number");
+            else if (userinput>number) return ("Number guessed is more than original number");
             else return ("You have guessed correctly");
             }
         }
